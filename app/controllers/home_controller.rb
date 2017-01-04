3b5f2dcdec5ba8a293f3518bc7956abc9a8016ff
RaviMariya-BTC <ravi.mariya@botreetechnologies.com>
2017-01-04 18:08:35 +0530

    applied the new theme in the application

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,5 @@
 class HomeController < ApplicationController
   def index
+    @testimonies = Testimony.all
   end
 end
--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -8,4 +8,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
+Rails.application.config.assets.precompile += ['property-listing.css', 'add-edit-property.css', 'property-details.css', 'index.css', 'jquery.youtubebackground.js', 'owl.carousel.css', 'owl.carousel.min.js', 'admin-style.css', 'common-pages.css']
