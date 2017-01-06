af9736dc48a79e306490442cc5f0575e2c1949ee
Bharat Soni <bharat.soni@botreetechnologies.com>
2017-01-06 13:27:19 +0530

    Sold properties should not visible on home page but should be visible properties list and admin panel.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,7 @@
 class HomeController < ApplicationController
   def index
+    @properties_featured_one_to_three = Property.active.not_sold.by_featured([1,2,3]).order(:featured)
+    @properties_featured_four_to_six  = Property.active.not_sold.by_featured([4,5,6]).order(:featured)
+    @all_properties = Property.active.not_sold.by_featured([1,2,3,4,5,6]).order(:featured).map { |property| PropertySerializer.new(property, root: false) }
   end
 end
