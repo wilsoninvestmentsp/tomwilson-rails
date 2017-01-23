2825abcbd7b76c1ca4b9d4794da1b4c21f9fe26d
RaviMariya-BTC <ravi.mariya@botreetechnologies.com>
2017-01-23 18:35:47 +0530

    links are allowed blank on property creation

--- app/models/link.rb
+++ app/models/link.rb
@@ -3,7 +3,7 @@ class Link < ActiveRecord::Base
 	belongs_to :property
 
 	validates_presence_of :title,:property_id
-	validates :link, format: { with: /(^(https?:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix, message: "Please Enter Valid Link" }
+	validates :link, format: { with: /(^(https?:\/\/)?[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$)/ix, message: "Please Enter Valid Link" }, allow_blank: true
 	def formatted_link
 		(link[/\Ahttp:\/\//] || link[/\Ahttps:\/\//]) ? link : "http://#{link}"
 	end
