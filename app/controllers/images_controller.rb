37bfd1fc70daff2c972a718870b1bdf8a37da133
RaviMariya-BTC <ravi.mariya@botreetechnologies.com>
2017-02-01 18:32:57 +0530

    removed jumbtron from home page

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -34,5 +34,5 @@ class ImagesController < ApplicationController
       render json: nil,status: :unprocessable_entity
     end
   end
-	{"public_id"=>"ijieuihof31oj5ejbv5t", "version"=>1467759446, "signature"=>"bbe7731597c5062bdc543f6f3769249c5083fbf6", "width"=>960, "height"=>720, "format"=>"jpg", "resource_type"=>"image", "created_at"=>"2016-07-05T22:57:26Z", "tags"=>[], "bytes"=>181168, "type"=>"upload", "etag"=>"75df14731c97f6a94bb3140f8ebbf3fa", "url"=>"http://res.cloudinary.com/wambl/image/upload/v1467759446/ijieuihof31oj5ejbv5t.jpg", "secure_url"=>"https://res.cloudinary.com/wambl/image/upload/v1467759446/ijieuihof31oj5ejbv5t.jpg", "original_filename"=>"1-front-view-McKinley-Ave.2962"}
+  {"public_id"=>"ijieuihof31oj5ejbv5t", "version"=>1467759446, "signature"=>"bbe7731597c5062bdc543f6f3769249c5083fbf6", "width"=>960, "height"=>720, "format"=>"jpg", "resource_type"=>"image", "created_at"=>"2016-07-05T22:57:26Z", "tags"=>[], "bytes"=>181168, "type"=>"upload", "etag"=>"75df14731c97f6a94bb3140f8ebbf3fa", "url"=>"http://res.cloudinary.com/wambl/image/upload/v1467759446/ijieuihof31oj5ejbv5t.jpg", "secure_url"=>"https://res.cloudinary.com/wambl/image/upload/v1467759446/ijieuihof31oj5ejbv5t.jpg", "original_filename"=>"1-front-view-McKinley-Ave.2962"}
 end
