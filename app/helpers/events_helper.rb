1062dda99479ff8a3de2eb8538dfcf268067bd3a
RaviMariya-BTC <ravi.mariya@botreetechnologies.com>
2017-04-05 11:47:03 +0530

    fixed timezone problem in event time

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,6 +1,9 @@
 module EventsHelper
-  def event_time(event_t)
-    time = DateTime.strptime(event_t.to_s, '%Q').new_offset('-0700')
+  def event_time(event_t, utc_offset)
+    time_zone = utc_offset < 0 ? '-%H:%M' : '+%H:%M'
+    utc_offset = (utc_offset.abs.to_s.first(5).to_i) 
+    hours = Time.zone.at(utc_offset).strftime(time_zone)
+    time = DateTime.strptime(event_t.to_s, '%Q').new_offset(hours)
     time.strftime('%B %d, %Y %l:%M %p')
   end
 end
