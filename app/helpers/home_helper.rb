ccd06d520d612319f83e079eca43eda27fe85d96
RaviMariya-BTC <ravi.mariya@botreetechnologies.com>
2017-05-16 17:42:42 +0530

    hide featured property section if property is not present

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -9,4 +9,21 @@ module HomeHelper
         'yellow'
     end
   end
+
+  def section_bg_class(properties, section)
+    case section
+      when 'magazine'
+        if properties
+          'grey-bg bdr-top-btm'
+        else 
+          'white-bg'
+        end
+      when 'feedback'
+        if properties
+          'white-bg'
+        else
+          'grey-bg'
+        end
+    end
+  end
 end
