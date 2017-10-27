406ed6cd3c8b382099ea48d0bf997fa0da4843ab
Sanjay Prajapati <sanjay.prajapati@botreetechnologies.com>
2017-10-27 11:27:34 +0530

    Add 'Where did you hear from option and send to Zoho CRM', Changes in contact us forms

--- app/assets/javascripts/AngularCTRL/ticket.js
+++ app/assets/javascripts/AngularCTRL/ticket.js
@@ -1,6 +1,5 @@
 App.controller('TicketCtrl',['$scope','$interval','$upload','$routeParams','$http','$uibModal',function($scope,$interval,$upload,$routeParams,$http,$uibModal){
 	JP('Ticket');
-
 	var scope = $scope;
 	scope.show_form = true;
 	scope.inquiry = {};
@@ -8,8 +7,10 @@ App.controller('TicketCtrl',['$scope','$interval','$upload','$routeParams','$htt
     $("#property-contact").find(':checkbox').trigger('click');
     $("#contact-form").find(':checkbox').trigger('click');
     $("#home-contact").find(':checkbox').trigger('click');
+    $(".main_contact").find("option").eq(0).attr('label','Please Select Option').val('Please Select Option').text('Please Select Option')
   };
-
+  scope.inquiry.hear_from_options = ['Where did you hear from us?', "Web Search", "Event", "Print Media",'Radio Show','Referral','Loopnet','Bigger Pockets','Social Media'];
+  scope.inquiry.hear_from = scope.inquiry.hear_from_options[0];
 	scope.moment = moment;
 	scope.submitForm = function(){
 
@@ -23,14 +24,20 @@ App.controller('TicketCtrl',['$scope','$interval','$upload','$routeParams','$htt
 		.then(function(response) {
 			scope.inquiry = null;
 			$('#home-contact').modal('hide');
+			$('#paradise-home-contact').modal('hide');
+			$('#inquiry-form').hide();
 			scope.openPopup('success', 'Thank You! Your ticket was successfully submitted!');
 
-			delete scope.show_form;
+			//delete scope.show_form;
+			scope.inquiry = {};
+			scope.inquiry.hear_from_options = ['Where did you hear from us?', "Web Search", "Event", "Print Media",'Radio Show','Referral','Loopnet','Bigger Pockets','Social Media'];
+  		scope.inquiry.hear_from = scope.inquiry.hear_from_options[0];
 			delete scope.loading;
 
 		},
 		function(response){
 			$('#home-contact').modal('hide');
+			$('#paradise-home-contact').modal('hide');
 			scope.openPopup('danger', 'Something Went Wrong!');
 			delete scope.loading;
 			scope.submitForm = null;
