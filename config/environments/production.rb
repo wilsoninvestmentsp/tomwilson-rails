5bba7c2bd9398120124fe9f87c06c3bfffa3f669
sanjay-btc <sanjay.prajapati@botreetechnologies.com>
2019-02-08 16:45:33 +0530

    Added changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,6 +89,7 @@ Rails.application.configure do
   config.middleware.insert_before(Rack::Runtime, Rack::Rewrite) do
     # Redirect everything from tomwilsonproperties.com to www.wilsoninvest.com with 301 Moved Permanently
     r301 /.*/, 'http://www.wilsoninvest.com$&', host: 'tomwilsonproperties.com'
+    r301 /.*/, 'http://www.wilsoninvest.com$&', host: 'www.tomwilsonproperties.com'
   end
 
   # MailChimp
